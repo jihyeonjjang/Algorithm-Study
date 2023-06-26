c89ce23955f80ea44f5946ec43ad5a7204b787bc
muunioi <muunii06@gmail.com>
2023-06-26 15:50:05 +0900

    feat : [BOJ] 11718 그대로 출력하기 / 출력초과

---
+++ muunioi/BAEKJOON/io/b11718.java
@@ -0,0 +1,15 @@
+package BAEKJOON.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while((input=br.readLine()) != null || (input=br.readLine()) != ""){
+            System.out.println(input);
+        }
+    }
+}

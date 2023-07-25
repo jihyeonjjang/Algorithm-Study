ee1e0286872764099e423d9a02cd7a9d28ec3d42
JiHyeonJang <76553061+jihyeonjjang@users.noreply.github.com>
2023-07-25 11:52:29 +0900

    feat : [BOJ] 15552 빠른 A+B / b5

---
+++ jihyeonjjang/BAEKJOON/sprout/b15552.swift
@@ -0,0 +1,69 @@
+// 시간 : 332 ms
+// 출처 : https://dev-mandos.tistory.com/45
+
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+// 풀이
+var answer = ""
+func solution() {
+    let a = file.readInt(), b = file.readInt()
+    answer += "\(a + b)\n"
+}
+
+let file = FileIO()
+
+let n = file.readInt()
+for _ in 0..<n {
+    solution()
+}
+
+print(answer)

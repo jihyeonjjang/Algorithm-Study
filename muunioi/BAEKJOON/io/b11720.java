959cb15bc303b2dd4089cdcc660bde9c1021bf63
muunioi <muunii06@gmail.com>
2023-07-27 14:51:45 +0900

    feat : [BOJ] 11720 숫자의 합

---
+++ muunioi/BAEKJOON/io/b11720.java
@@ -0,0 +1,18 @@
+package BAEKJOON.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int result = 0;
+        for(int i=0; i<n; i++){
+            result = result + ( s.charAt(i) - '0' );
+        }
+        System.out.println(result);
+    }
+}

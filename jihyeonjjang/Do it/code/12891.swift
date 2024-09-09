fc6e9e4bc4b8972b65e02fa21d9ea5af1c85864a
jihyeonjjang <jihyeon9975@gmail.com>
2024-09-09 15:50:33 +0900

    Refactor : [BOJ] 12891 DNA 비밀번호 / s2 / Improved code readability and maintainability by modularizing functions

--- jihyeonjjang/Do it/code/12891.swift
+++ jihyeonjjang/Do it/code/12891.swift
@@ -15,74 +15,61 @@ var result = 0
 var checkSecret = 0
 var myArr = [Int](repeating: 0, count: 4) // 현재 상태 배열
 
-for i in input[1]...input[0] {
-    var j = i-input[1]
-    checkSecret = 0
-    
-    if j == 0 {
-        for i in j..<input[1] {
-            switch strArray[i] {
-                case "A":
-                    myArr[0] += 1
-                case "C":
-                    myArr[1] += 1
-                case "G":
-                    myArr[2] += 1
-                case "T":
-                    myArr[3] += 1
-                default:
-                    print("DNA 문자열이 아닙니다.")
-            }
-        }
-        
-        for i in 0...3 {
-            if myArr[i] >= checkArr[i] {
-                checkSecret += 1
-            }
-        }
-        
-        if checkSecret == 4 {
-            result += 1
-        }
-        
-    } else {
-        switch strArray[j-1] {
-            case "A":
-                myArr[0] -= 1
-            case "C":
-                myArr[1] -= 1
-            case "G":
-                myArr[2] -= 1
-            case "T":
-                myArr[3] -= 1
-            default:
-                print("DNA 문자열이 아닙니다.")
-        }
-        
-        switch strArray[j+input[1]-1] {
-            case "A":
-                myArr[0] += 1
-            case "C":
-                myArr[1] += 1
-            case "G":
-                myArr[2] += 1
-            case "T":
-                myArr[3] += 1
-            default:
-                print("DNA 문자열이 아닙니다.")
-        }
-        
-        for i in 0...3 {
-            if myArr[i] >= checkArr[i] {
-                checkSecret += 1
-            }
-        }
-        
-        if checkSecret == 4 {
-            result += 1
-        }
+for i in checkArr {
+    if i == 0 {
+        checkSecret += 1
     }
 }
 
+for i in 0..<input[1] {
+    add(i: i)
+}
+if checkSecret == 4 { result += 1 }
+
+for i in input[1]..<input[0] {
+    var j = i-input[1]
+    remove(i: j)
+    add(i: j+input[1])
+    if checkSecret == 4 { result += 1 }
+}
+
 print(result)
+
+func add (i: Int) {
+    switch strArray[i] {
+    case "A":
+        myArr[0] += 1
+        if myArr[0] == checkArr[0] { checkSecret += 1}
+    case "C":
+        myArr[1] += 1
+        if myArr[1] == checkArr[1] { checkSecret += 1}
+    case "G":
+        myArr[2] += 1
+        if myArr[2] == checkArr[2] { checkSecret += 1}
+    case "T":
+        myArr[3] += 1
+        if myArr[3] == checkArr[3] { checkSecret += 1}
+    default:
+        print("DNA 문자열이 아닙니다.")
+    }
+}
+
+func remove(i: Int) {
+    switch strArray[i] {
+    case "A":
+        if myArr[0] == checkArr[0] { checkSecret -= 1}
+        myArr[0] -= 1
+    case "C":
+        if myArr[1] == checkArr[1] { checkSecret -= 1}
+        myArr[1] -= 1
+    case "G":
+        if myArr[2] == checkArr[2] { checkSecret -= 1}
+        myArr[2] -= 1
+    case "T":
+        if myArr[3] == checkArr[3] { checkSecret -= 1}
+        myArr[3] -= 1
+    default:
+        print("DNA 문자열이 아닙니다.")
+    }
+}
 

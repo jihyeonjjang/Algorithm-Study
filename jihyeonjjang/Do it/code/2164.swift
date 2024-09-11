4c7d3dbf5311ac371bfc8590d3ccc7b32607ed19
jihyeonjjang <jihyeon9975@gmail.com>
2024-09-11 15:58:51 +0900

    Refactor : [BOJ] 2164 카드2 / s4 / Refactor code to resolve timeout issue

--- jihyeonjjang/Do it/code/2164.swift
+++ jihyeonjjang/Do it/code/2164.swift
@@ -7,14 +7,20 @@
 
 import Foundation
 let num = Int(readLine()!)!
-var array = [Int]()
-for i in 1...num {
-    array.append(i)
-}
+var queue = Array(1...num)
+var temp = 0
 
-while array.count > 1 {
-    array.removeFirst()
-    array.append(array.removeFirst())
+if num == 1 { print(1) }
+else {
+    while queue[temp] > 0 {
+        queue[temp] = 0
+        if queue.count == temp+2 {
+            break
+        }
+        queue.append(queue[temp+1])
+        queue[temp+1] = 0
+        temp += 2
+    }
+    
+    print(queue.removeLast())
 }
-
-print(array[0])
